5bcc2a974ed740714be18780816a6862204db768
Armaan <iamarmaankhan07@gmail.com>
2024-09-13 18:23:30 +0530

    favicon try && title say it

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,31 +1,36 @@
-import {
-  Box,
-  Container,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-  Text,
-} from "@chakra-ui/react";
-import { useEffect } from "react";
+import React, { useEffect } from 'react';
+import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text, IconButton } from "@chakra-ui/react";
+import { FaGithub } from 'react-icons/fa'; // Import GitHub icon
 import { useNavigate } from 'react-router-dom';
 import Login from "../components/Authentication/Login";
 import Signup from "../components/Authentication/Signup";
 
 function Homepage() {
-  const navigate = useNavigate(); // Updated from 'history' to 'navigate'
+  const navigate = useNavigate();
 
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("userInfo"));
 
-    if (user) navigate("/chats"); // Updated 'history' to 'navigate'
-  }, [navigate]); // Correctly using 'navigate' in the dependency array
+    if (user) navigate("/chats");
+  }, [navigate]);
 
   return (
     <Container maxW="xl" centerContent>
+      {/* GitHub Button */}
+      <Box position="absolute" top="10px" right="10px">
+        <IconButton
+          icon={<FaGithub />}
+          aria-label="GitHub"
+          fontSize="30px"
+          onClick={() => window.open("https://github.com/armaankhan07/mern-chat-app", "_blank")} // Change URL to your GitHub profile or project
+          variant="ghost"
+          color="white" // Default color of the GitHub icon
+          _hover={{ color: "gray.400" }} // Hover effect to change icon color to light gray
+        />
+      </Box>
+
       <Box
-        display="flex" // Updated 'd' to 'display' for better readability
+        display="flex"
         justifyContent="center"
         p={3}
         bg="white"
@@ -35,7 +40,7 @@ function Homepage() {
         borderWidth="1px"
       >
         <Text fontSize="4xl" fontFamily="Work sans">
-          Talk-A-Tive
+          Say It
         </Text>
       </Box>
       <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
